75b345b5067162f61037d2f044126ecce4e73464
Yunus Emre AKBULUT <yemreakbulut@hotmail.com>
2021-12-27 00:17:11 +0300

    Step 4: Using DotNet Core HealthCheck Service

---
+++ BestPractices.API/Extensions/HealthCheckConfigureExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
+
+namespace BestPractices.API.Extensions
+{
+    public static class HealthCheckConfigureExtension
+    {
+        public static IApplicationBuilder UseCustomHealthCheck(this IApplicationBuilder app)
+        {
+            //Here create a fake api url for Healthcheck and return "OK"
+            app.UseHealthChecks("/api/health", new HealthCheckOptions()
+            {
+                ResponseWriter = async (context, report) =>
+                {
+                    await context.Response.WriteAsync("OK");
+                }
+            });
+
+            return app;
+        }
+    }
+}
--- BestPractices.API/Startup.cs
+++ BestPractices.API/Startup.cs
@@ -1,16 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BestPractices.API.Extensions;
 
 namespace BestPractices.API
 {
@@ -32,10 +26,13 @@ namespace BestPractices.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BestPractices.API", Version = "v1" });
             });
+
+            //Add to Healthcheck service
+            services.AddHealthChecks();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -43,6 +40,9 @@ namespace BestPractices.API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BestPractices.API v1"));
             }
+
+            //Custom HealthCheck Configure File include in project
+            app.UseCustomHealthCheck();
 
             app.UseHttpsRedirection();
 
